b3cffcaa07f294a49b6e9d26a1d086c73738e301
Harrison Roth <rothhl@miamioh.edu>
2016-10-08 10:19:01 -0400

    Extention works, Yay

--- extension/run2.js
+++ extension/run2.js
@@ -1,8 +1,10 @@
+var id;
+
 console.log("We made it");
 
 if($("[type=password]").length){
 	chrome.storage.local.get('id', function(profileObj) {
-		var id = profileObj.id;
+		id = profileObj.id;
 		console.log(id);
 	  	if (typeof id === "undefined") {
 	  		console.log("Uninitialized user")
@@ -10,32 +12,35 @@ if($("[type=password]").length){
 	  		$.ajax({
 	  			url: "https://passtap.com/server.php?v1=checkDomain&v2=" + id + "&v3=" + document.domain, 
 	  			success: function(result){
-        		if(result==1){
-        			//has used domain
-        			checkFill();
-        		}else if(result==0){
-        			//hasn't used domain
-        			if (confirm('Would you like to generate a password for this site?')) {
-       	 				generatePass();
-    				}
-        		}else{
-        			//invalid id
-
+	  				console.log("checkDomain: "+ result);
+	        		if(result==1){
+	        			//has used domain
+	        			checkFill();
+	        		}else if(result==0){
+	        			//hasn't used domain
+	        			if (confirm('Would you like to generate a password for this site?')) {
+	       	 				generatePass();
+	    				}
+	        		}else{
+	        			//invalid id
+	        			chrome.storage.local.remove('id');
+	        			location.reload();
+	        		}
         		}
-        	}
-        });
+        	});
 	  		
 	  	}	
 
 });
 }
 
+	var timer;
 
 	function checkFill(){
   		$.ajax({
   			url: "https://passtap.com/server.php?v1=getPass&v2=" + id + "&v3=" + document.domain, 
   			success: function(result){
-    		$("[type=password]").val(result);
+  				timer = setInterval(check, 2000);
     	}});
     }
 
@@ -43,6 +48,26 @@ if($("[type=password]").length){
     	$.ajax({
   			url: "https://passtap.com/server.php?v1=generatePass&v2=" + id + "&v3=" + document.domain, 
   			success: function(result){
-    		$("[type=password]").val(result);
+  				timer = setInterval(check, 2000);
     	}});
+    }
+
+    function check(){
+    	$.ajax({
+  			url: "https://passtap.com/server.php?v1=checkPass&v2=" + id + "&v3=" + document.domain, 
+  			success: function(result){
+  				console.log("Return: " + result);
+  				if(result == 0){
+  					return;
+  				}
+  				last = $("input").first();
+  				$("input").each(function(){
+  					if($(this).attr("type") == "password"){
+  						last.val("User Name Here");
+  						clearInterval(timer);
+  					}
+  					last = $(this);
+  				});
+  				$("[type=password]").val(result);
+  			}});
     }

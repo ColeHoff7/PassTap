13ff437f9e1fc7c3a803d98ba1b126e03127d9df
Cole Hoffbauer <colehoff7@gmail.com>
2016-10-08 16:54:28 -0400

    potential fix for crash

--- PassTapAndroid/app/src/main/java/com/passtap/passtapandroid/InitializeBrowserActivity.java
+++ PassTapAndroid/app/src/main/java/com/passtap/passtapandroid/InitializeBrowserActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.TextView;
 
 import com.android.volley.Request;
@@ -31,7 +32,7 @@ public class InitializeBrowserActivity extends AppCompatActivity {
     }
 
     //called when finished with browser authentication
-    public void toMain(){
+    public void toMain(View view){
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
         finish();

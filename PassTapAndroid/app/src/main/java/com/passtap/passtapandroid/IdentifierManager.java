0763fddc9c3bacf36a9acf9240a3fa7165b54e1e
Cole Hoffbauer <colehoff7@gmail.com>
2016-10-08 12:15:18 -0400

    I've lost track what I've added. I only got 2 hours of sleep. I'm tired. Added some Firebase stuff at some point...

---
+++ PassTapAndroid/app/src/main/java/com/passtap/passtapandroid/IdentifierManager.java
@@ -0,0 +1,57 @@
+package com.passtap.passtapandroid;
+
+import android.util.Log;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.FirebaseInstanceIdService;
+
+import static com.google.android.gms.internal.zzs.TAG;
+
+/**
+ * Created by Cole on 10/8/16.
+ */
+
+public class IdentifierManager extends FirebaseInstanceIdService {
+    @Override
+    public void onTokenRefresh() {
+        // Get updated InstanceID token.
+        String refreshedToken = FirebaseInstanceId.getInstance().getToken();
+        Log.d(TAG, "Refreshed token: " + refreshedToken);
+
+        // If you want to send messages to this application instance or
+        // manage this apps subscriptions on the server side, send the
+        // Instance ID token to your app server.
+        sendNewTokenToServer(refreshedToken);
+    }
+
+    public void sendNewTokenToServer(String refreshedToken){
+        RequestQueue queue = Volley.newRequestQueue(this);
+        String url ="https://passtap.com/server.php?";
+        // Request a string response from the provided URL.
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        // Display the first 500 characters of the response string.
+                        if(response != null) {
+
+                        }else{
+
+                        }
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+            }
+        });
+        // Add the request to the RequestQueue.
+        queue.add(stringRequest);
+    }
+}
--- PassTapAndroid/app/src/main/java/com/passtap/passtapandroid/InitializeBrowserActivity.java
+++ PassTapAndroid/app/src/main/java/com/passtap/passtapandroid/InitializeBrowserActivity.java
@@ -1,7 +1,7 @@
 package com.passtap.passtapandroid;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
 
 import com.android.volley.Request;
@@ -10,8 +10,15 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
+import com.google.firebase.iid.FirebaseInstanceId;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class InitializeBrowserActivity extends AppCompatActivity {
+
+    //public static FirebaseInstanceIdService instanceIdService = new FirebaseInstanceIdService();
+    public static FirebaseInstanceId instanceId = FirebaseInstanceId.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,7 +33,9 @@ public class InitializeBrowserActivity extends AppCompatActivity {
 
         final TextView mTextView = (TextView) findViewById(R.id.output);
         RequestQueue queue = Volley.newRequestQueue(this);
-        String url ="https://passtap.com/";
+        String token = instanceId.getToken();
+        String url ="https://passtap.com/server.php?v1=generateKey&v2=";//thisisakey1&v3=";
+        url += token + "&v3=";
 
         // Request a string response from the provided URL.
         StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
@@ -35,7 +44,18 @@ public class InitializeBrowserActivity extends AppCompatActivity {
                     public void onResponse(String response) {
                         // Display the first 500 characters of the response string.
                         if(response != null) {
-                            mTextView.setText("Response is: " + response.substring(0, 500));
+                            String browserCode = "ERROR";
+                            String privateKey = null;
+                            try {
+                                JSONObject json = new JSONObject(response);
+                                browserCode = json.getString("access_token");
+                                privateKey = json.getString("private_key");
+
+
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                            }
+                            mTextView.setText("Browser Code: " + browserCode);
                         }else{
                             mTextView.setText("null");
                         }
@@ -46,7 +66,7 @@ public class InitializeBrowserActivity extends AppCompatActivity {
                 mTextView.setText("That didn't work!");
             }
         });
-// Add the request to the RequestQueue.
+        // Add the request to the RequestQueue.
         queue.add(stringRequest);
     }
 }

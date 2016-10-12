9d8bd439613570b73cd713a81cbd157ce3c477d8
Harrison Roth <rothhl@miamioh.edu>
2016-10-11 21:48:51 -0400

    active tabs in popup window

--- extension/run.js
+++ extension/run.js
@@ -5,34 +5,33 @@ function load(){
 	chrome.storage.local.get('id', function(profileObj) {
 		id = profileObj.id;
 		console.log(id);
+        chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, function (tabs) {
+                var url = tabs[0].url;
+                console.log(url);
+                //find & remove protocol (http, ftp, etc.) and get domain
+                if (url.indexOf("://") > -1) {
+                    domain = url.split('/')[2];
+                }
+                else {
+                    domain = url.split('/')[0];
+                }
+                //find & remove port number
+                domain = domain.split(':')[0];
+                console.log(domain);
+        });
 	  	if (typeof id === "undefined") {
     // No profile in storage
     	console.log("I kinda work");
     	document.getElementById("content").innerHTML= "<h3>First Time User?</h3> <p>Please enter the ID generated from" +
     	" the PassTap mobile app.<p> " +
     	"<input style=\"max-width:100\%\" id=\"ID\" type=\"text\">";
-		
 
-  	} else {
-    // Profile exists in storage
-    	$("#submit").attr("hidden","true");
-        $('#reset').show();
-        chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, function (tabs) {
-            var url = tabs[0].url;
-            console.log(url);
-            //find & remove protocol (http, ftp, etc.) and get domain
-            if (url.indexOf("://") > -1) {
-                domain = url.split('/')[2];
-            }
-            else {
-                domain = url.split('/')[0];
-            }
+      	} else {
+        // Profile exists in storage
+        	$("#submit").attr("hidden","true");
+            $("#navigation").show();
 
-            //find & remove port number
-                domain = domain.split(':')[0];
-                console.log(domain);
-        });
-  	}
+      	}
 	});
 }
 
@@ -85,9 +84,14 @@ function resetPass(){
 
 function settings(){
     $("#notSettings").attr("hidden","true");
-    $("#Settings").show();
+    $("#navigation").show();
+    $('#tabs').show();
+    $('#first').removeClass("active");
+    $('#sets').addClass("active");
 
 }
+
+
 
 function reload(){
 	document.getElementById("content").innerHTML = "<p>Please enter the value generated from" +
@@ -95,9 +99,36 @@ function reload(){
     	"<input style=\"max-width:100\%\" id=\"ID\" type=\"text\">";
 }
 
+$('ul.nav-tabs li a').click(function (e) {
+    $('ul.nav-tabs li.active').removeClass('in active');
+    $(this).parent('li').addClass('active');
+    var num = this.getAttribute("value");
+    console.log(num);
+    var val = 'tab' + num;
+    console.log(val);
+    $("div[id="+ val + "]").addClass('active');
+    $('div.active').removeClass('active');
+    $('#'+val).addClass('active');
+});
 
+// function first(){
+//     $('#tab1').addClass('active');
+// }
+
+// function second(){
+//     $('#tab2').addClass('active');
+// }
+
+// function third(){
+//     $('#tab3').addClass('active');
+//     console.log("here");
+// }
 
 load();
 document.getElementById("button").addEventListener('click', save);
 document.getElementById("res").addEventListener('click', resetPass);
-document.getElementById("set").addEventListener('click', settings);
+document.getElementById("set").addEventListener('click', settings); 
+// document.getElementById("1").addEventListener('click', first);
+// document.getElementById("2").addEventListener('click', second);
+// document.getElementById("3").addEventListener('click', third);
+
--- extension/run2.js
+++ extension/run2.js
@@ -77,3 +77,5 @@ if($("[type=password]").length){
   				$("[type=password]").val(data["password"]);
   			}});
     }
+
+    
